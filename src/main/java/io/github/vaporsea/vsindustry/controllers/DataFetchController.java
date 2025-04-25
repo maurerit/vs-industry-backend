56bb65fe7d0ecf80a1e2b314aff268823b09f498
Matthew Maurer <maurer.it@gmail.com>
2025-04-24 20:04:27 -0400

    Starts of an implementation
    This is actually the 'meat of it' but we now need all the fluff that is required which we gave up to javascript... which we're now taking over... or... OR... we get really weird and make this depend on that and we can get this from that and be done with it :D.  But that doesn't have this so we're boned either way.  Time to take this over, need to rethink things maybe... OR... I wonder if one of these ai agents

--- src/main/java/io/github/vaporsea/vsindustry/controllers/DataFetchController.java
+++ src/main/java/io/github/vaporsea/vsindustry/controllers/DataFetchController.java
@@ -1,15 +1,13 @@
 package io.github.vaporsea.vsindustry.controllers;
 
 import io.github.vaporsea.vsindustry.contract.MarketOrderSummaryDTO;
+import io.github.vaporsea.vsindustry.service.DataFetchService;
+import io.github.vaporsea.vsindustry.service.DataSyncScheduler;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import io.github.vaporsea.vsindustry.service.DataFetchService;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-
-import java.util.concurrent.atomic.AtomicBoolean;
 
 /**
  * @author Matt Maurer <br>
@@ -20,28 +18,27 @@ import java.util.concurrent.atomic.AtomicBoolean;
 @RestController
 public class DataFetchController {
     
-    private final AtomicBoolean isFetching = new AtomicBoolean(false);
-    
     private final DataFetchService dataFetchService;
+    private final DataSyncScheduler dataSyncScheduler;
     
     @PostMapping("/data/fetch-all")
     public void fetch() {
-        if (isFetching.get()) {
+        if (dataSyncScheduler.isFetching()) {
             log.warn("Data fetch already in progress");
             return;
         }
         
-        isFetching.set(true);
+        dataSyncScheduler.setFetching(true);
         dataFetchService.fetchMarketTransactions();
         dataFetchService.fetchJournalEntries();
         dataFetchService.fetchIndustryJobs();
         dataFetchService.fetchContracts();
-        isFetching.set(false);
+        dataSyncScheduler.setFetching(false);
     }
     
     @GetMapping("/data/fetch-status")
     public String fetchStatus() {
-        return isFetching.get() ? "{ \"status\": \"Fetching in progress\" }" :
+        return dataSyncScheduler.isFetching() ? "{ \"status\": \"Fetching in progress\" }" :
                 "{ \"status\": \"No fetch in progress\" }";
     }
     
